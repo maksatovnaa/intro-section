69475278b711c1ab2ba4962c445784fa3a067d9c
tazhmurat safiyanur <sakibaeva2024@MacBook-Air-Maksatovna.local>
2025-05-11 20:09:19 +0500

    first commit

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
-document.getElementById('featuresBtn').addEventListener('mouseover', function () {
-  let menu = document.getElementById('featuresMenu');
-  if (!menu) {
-    let newMenu = document.createElement('div');
-    newMenu.id = 'featuresMenu';
-    newMenu.className = 'absolute top-8 w-48 bg-white rounded-lg shadow-lg p-4 z-10';
-    newMenu.innerHTML = `
-      <div class="flex items-center gap-3 mb-3">
-        <img src="icon-todo.svg"  class="w-4 h-4">
-        <p class="text-gray-600 text-sm">Todo List</p>
-      </div>
-      <div class="flex items-center gap-3 mb-3">
-        <img src="icon-calendar.svg" class="w-4 h-4">
-        <p class="text-gray-600 text-sm">Calendar</p>
-      </div>
-      <div class="flex items-center gap-3 mb-3">
-        <img src="icon-reminders.svg" class="w-4 h-4">
-        <p class="text-gray-600 text-sm">Reminders</p>
-      </div>
-      <div class="flex items-center gap-3">
-        <img src="icon-planning.svg" class="w-4 h-4">
-        <p class="text-gray-600 text-sm">Planning</p>
-      </div>
-    `;
-    document.getElementById('featuresBtn').parentElement.appendChild(newMenu);
+document.getElementById('burger').addEventListener('click', function () {
+  document.getElementById('navbar').classList.toggle('active');
+  this.classList.toggle('burger-active');
+});
+
+featuresBtn.addEventListener('click', function () {
+  if (featuresMenu.style.display === 'flex') {
+    featuresMenu.style.display = 'none';
   } else {
-    menu.remove();
+    featuresMenu.style.display = 'flex';
+    companyMenu.style.display = 'none';
   }
 });
 
-document.getElementById('companyBtn').addEventListener('mouseover', function () {
-  let menu = document.getElementById('companyMenu');
-  if (!menu) {
-    let newMenu = document.createElement('div');
-    newMenu.id = 'companyMenu';
-    newMenu.className = 'absolute top-8 w-40 bg-white rounded-lg shadow-lg p-4 z-10';
-    newMenu.innerHTML = `
-      <p class="text-gray-600 text-sm mb-3">History</p>
-      <p class="text-gray-600 text-sm mb-3">Our Team</p>
-      <p class="text-gray-600 text-sm">Blog</p>
-    `;
-    document.getElementById('companyBtn').parentElement.appendChild(newMenu);
+companyBtn.addEventListener('click', function () {
+  if (companyMenu.style.display === 'flex') {
+    companyMenu.style.display = 'none';
   } else {
-    menu.remove();
+    companyMenu.style.display = 'flex';
+    featuresMenu.style.display = 'none';
   }
 });
